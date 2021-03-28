efde9ad47619d63dcb160d3545017c00d582bd9e
saurav2267 <55321375+saurav2267@users.noreply.github.com>
2021-03-28 09:56:00 +0530

    Add files via upload

---
+++ src/ColorContext.js
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+const ColorContext = createContext(["aqua", () => {}]);
+export default ColorContext;
---
+++ src/Video.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "@reach/router";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
+
+TimeAgo.addDefaultLocale(en);
+
+const Video = (props) => {
+  const timeAgo = new TimeAgo("en-us");
+  const dateAdded = new Date(props.dateAdded);
+  return (
+    <Link to={`/watch/${props.id}`} className="video-container">
+    <div className="video-container">
+      <div className="video-image">
+        <img src={props.thumbnail.url} alt={props.title} />
+      </div>
+      <div className="video-info">
+        <h3>{props.title}</h3>
+        <p>{timeAgo.format(dateAdded)}</p>
+        <h4>{props.channel}</h4>
+        <p>{props.description}</p>
+      </div>
+    </div>
+    </Link>
+  );
+};
+
+export default Video;
